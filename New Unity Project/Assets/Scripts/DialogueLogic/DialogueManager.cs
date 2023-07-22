8107b5933868097a673b3a6aa3dd39ffdb94be7c
MrBlueStudios <80816139+MrBlueStudios@users.noreply.github.com>
2023-07-22 13:01:05 +0200

    commitie for new npc assets & dialogue scripts
    importante commit

---
+++ New Unity Project/Assets/Scripts/DialogueLogic/DialogueManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+
+// singleton DialogueManager class
+public class DialogueManager : MonoBehaviour
+{
+    // static instance of the DialogueManager class
+    public static DialogueManager Instance { get; private set; }
+
+    private Queue<string> sentences;
+
+    private void Start()
+    {
+        // Initialize the static instance of the DialogueManager class
+        Instance = this;
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        // Clear the sentences queue
+        sentences.Clear();
+        // Enqueue all the sentences from the dialogue
+        foreach (var sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        // Display the first sentence
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        // Check if there are any sentences left in the queue
+        if (sentences.Count == 0)
+        {
+            // End the dialogue
+            EndDialogue();
+            return;
+        }
+        // Dequeue the next sentence
+        string sentence = sentences.Dequeue();
+        // Display the sentence
+        Debug.Log(sentence);
+    }
+
+    public void EndDialogue()
+    {
+          // Implement the end of the dialogue
+        Debug.Log("End of dialogue");
+    }
+}
---
+++ New Unity Project/Assets/Scripts/DialogueLogic/DialogueTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class  DialogueTrigger : MonoBehaviour
+{
+    [SerializeField] private Dialogue dialogue;
+
+    public void TriggerDialogue()
+    {
+        // Start the dialogue
+        DialogueManager.Instance.StartDialogue(dialogue);
+    }
+}
